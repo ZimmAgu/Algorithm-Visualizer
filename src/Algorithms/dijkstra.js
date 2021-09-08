2788b2756987c7254f0255c008efd3aa5dc077f3
Zim Agu <zimmagu@gmail.com>
2021-09-07 18:59:54 -0700

    Start node distance is set to 0 and all other node distances are set to infinity

--- src/Algorithms/dijkstra.js
+++ src/Algorithms/dijkstra.js
@@ -1,11 +1,38 @@
 function dijkstra (graph, startNode, endNode) {
-    console.log(endNode)
-    // // Step 1: 
+    const allNodesInGraph = getAllNodes(graph);
+    setInitialNodeDistance(allNodesInGraph, startNode)
+    console.log(graph)
 
-    // let distances = {};                 // // tracks distances from the start node using an object
-    // distances[endNode] = Infinity;      // The distances of all of the non start nodes are initially set to infinity
     
 }
+
+
+function getAllNodes (grid) {
+    const nodes = [];
+    for (const row of grid) {
+      for (const node of row) {
+        nodes.push(node);
+      }
+    }
+    return nodes;
+}
+
+function setInitialNodeDistance (nodes, startNode) {
+    nodes.forEach(node => {
+        if (node === startNode) {
+            node.distance = 0;
+        } else {
+            node.distance = Infinity;
+        }
+    })
+}
+  
+
+
+
+
+
+
 
 export default dijkstra;
 
