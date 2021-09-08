6e67f35397fff8ab984f54857da1cd1cf6721987
Zim Agu <zimmagu@gmail.com>
2021-09-07 19:25:35 -0700

    The starting node gets shifted first

--- src/Algorithms/dijkstra.js
+++ src/Algorithms/dijkstra.js
@@ -1,8 +1,14 @@
 function dijkstra (graph, startNode, endNode) {
-    const allNodesInGraph = getAllNodes(graph);
-    setInitialNodeDistance(allNodesInGraph, startNode)
-    console.log(graph)
+    const visitedNodesInOrder = [];
 
+    const unvisitedNodes = getAllNodes(graph);
+    setInitialNodeDistance(unvisitedNodes, startNode)
+
+    while (!!unvisitedNodes.length) {
+        sortNodesByDistance(unvisitedNodes);   
+        const nearestNode = unvisitedNodes.shift(); // The starting node will always have a distance of 0 so the starting node should always be shifted first 
+        console.log(nearestNode);
+    }
     
 }
 
@@ -17,17 +23,21 @@ function getAllNodes (grid) {
     return nodes;
 }
 
+
+
 function setInitialNodeDistance (nodes, startNode) {
     nodes.forEach(node => {
-        if (node === startNode) {
+        if (node === startNode) {   // Sets the distance of the start node to 0
             node.distance = 0;
-        } else {
+        } else {                    // Sets the distance of every other node to infinity
             node.distance = Infinity;
         }
     })
 }
   
-
+function sortNodesByDistance(unvisitedNodes) { // Sorts nodes based on distance (smallest to largest)
+    unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
+}
 
 
 
