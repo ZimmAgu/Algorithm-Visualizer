98633dc178e48e7ce6f3285e579f2933a12ae45e
Zim Agu <zimmagu@gmail.com>
2021-09-09 11:16:22 -0700

    I now have a way of getting unvisited neighbors for dijkstra

--- src/Algorithms/dijkstra.js
+++ src/Algorithms/dijkstra.js
@@ -4,10 +4,11 @@ function dijkstra (graph, startNode, endNode) {
     const unvisitedNodes = getAllNodes(graph);
     setInitialNodeDistance(unvisitedNodes, startNode)
 
-    while (!!unvisitedNodes.length) {
+    while (!!unvisitedNodes.length) { // After every shift, the amount of unvisited nodes will decrease by 1 until there are none left
         sortNodesByDistance(unvisitedNodes);   
         const nearestNode = unvisitedNodes.shift(); // The starting node will always have a distance of 0 so the starting node should always be shifted first 
-        console.log(nearestNode);
+        nearestNode.isVisited = true;
+        getUnvisitedNeighbors(nearestNode, graph)
     }
     
 }
@@ -40,7 +41,34 @@ function sortNodesByDistance(unvisitedNodes) { // Sorts nodes based on distance 
 }
 
 
+function getUnvisitedNeighbors(node, grid) {    // Gets the nearest unvisited neighbor in each direction of the current node
+    const neighbors = [];
+    const {row, column} = node; // Retrieves the row and column value from the nearest node
+    
 
+    if (row > 0) {                               // As long as the above neighbor doesn't go off the grid (off the grid being a row less than 0)
+        neighbors.push(grid[row - 1][column]);   // Gets the neighbor directly above the current node (row - 1)
+    }
+
+    if (row < grid.length - 1) {                 // As long as the below neighbor doesn't go off the grid (off the grid being larger than the length of the grid). Keep in mind that counting starts at 0 so grid - 1 is actually the length of the grid 
+        neighbors.push(grid[row + 1][column]);   // Gets the neighbor directly below the current node (row + 1)
+    }
+
+    if (column > 0) {                            // As long as the above neighbor doesn't go off the grid (off the grid being a column less than 0)
+        neighbors.push(grid[row][column - 1]);   // Gets the neighbor directly to the left of the current node (column - 1)
+    } 
+
+    if (column < grid[0].length - 1) {           // As long as the below neighbor doesn't go off the grid (off the grid being larger than the height of the grid). Keep in mind that counting starts at 0 so grid - 1 is actually the height of the grid 
+        neighbors.push(grid[row][column + 1]); 
+    } 
+
+    // const result = neighbors.filter(neighbor => !neighbor.isVisited);
+    return neighbors.filter(neighbor => !neighbor.isVisited);   // Returns neighbors that have not been visited
+
+    // console.log(result);
+    // console.log(neighbors)
+
+}
 
 
 
