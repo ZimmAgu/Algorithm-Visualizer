facca7e736f4f1dc7a15c39b3c6519885283740e
Zim Agu <zimmagu@gmail.com>
2021-10-07 15:13:44 -0700

    Algorithms are now chosen from the navigation bar

--- src/WebPresentation/NavigationBar.js
+++ src/WebPresentation/NavigationBar.js
@@ -1,29 +1,40 @@
 import React from 'react'
 import { Button, Container, Dropdown, Navbar } from 'react-bootstrap';
 import { visualizeDijkstra } from '../Animations/algorithmAnimations';
-
+import { useState, useEffect, useRef } from 'react';
+import dijkstra from '../Algorithms/dijkstra';
 
 
 
 function NavigationBar (props) {
+    const [currentAlgorithm, setCurrentAlgorithm] = useState(null);
+    const [navButtonText, setNavButtonText] = useState('Choose your Algorithm');
+
+
     function handleButtonClick () {
-        visualizeDijkstra(props.gridState)
+        if (currentAlgorithm == 'dijkstra') {
+            visualizeDijkstra(props.gridState)  
+        }
     }
 
+    function handleDijkstraDropdown () {
+        setCurrentAlgorithm('dijkstra')
+        setNavButtonText('Visualize Dijkstras Algorithm')
+    }
 
     return (
         <>
             <Navbar expand="lg" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="">Algorithm Visualizer</Navbar.Brand>
-                    <button onClick={handleButtonClick}>Visualize Dijkstra's Algorithm</button>
+                    <button onClick={handleButtonClick}>{navButtonText}</button>
                     <Dropdown>
                         <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Dropdown Button
+                            Algorithms
                         </Dropdown.Toggle>
 
                         <Dropdown.Menu>
-                            <Dropdown.Item href="">Dijkstra</Dropdown.Item>
+                            <Dropdown.Item onClick={handleDijkstraDropdown}>Dijkstra</Dropdown.Item>
                             <Dropdown.Item href="">Another action</Dropdown.Item>
                             <Dropdown.Item href="">Something else</Dropdown.Item>
                         </Dropdown.Menu>
